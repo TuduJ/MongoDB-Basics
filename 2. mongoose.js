78744dcbf4f99fa9f50dc77b3f6ba1775d3c3676
Abhishek Sarwan <abhisheksarwan@icloud.com>
2020-04-01 23:06:28 +0530

    Add files via upload

---
+++ 2. mongoose.js
@@ -0,0 +1,46 @@
+var mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/dogapp", {useNewUrlParser: true, useUnifiedTopology: true});    //connect
+
+var dogSchema = new mongoose.Schema({               //creating a schema
+    name: String,
+    age: Number,
+    breed: String
+});
+
+var Dog = mongoose.model("Dogy", dogSchema);         //creating a model //Dog is the single version of the model  //dogy pluralised will be the collection name
+
+var Tom = new Dog({             
+    name: "Tom",
+    age: 10,
+    breed: "Mutt"
+});
+
+Tom.save(function(err, dogs){            //adding the dog
+    if(err){
+        console.log("Wrong!");
+    } else {
+        console.log("We added a Dog!");
+        console.log(dogs);
+    }
+});
+
+Dog.create({                            //combined
+    name:"Matt",
+    age: 10,
+    breed: "Mutt"
+}, function(err, dog){
+    if(err){
+        console.log(err);
+    } else {
+        console.log(dog);
+    }
+})
+
+Dog.find({}, function(err, dog){       //show data
+    if(err){
+        console.log("Wrong!");
+    } else {
+        console.log(dog);
+    }
+});
